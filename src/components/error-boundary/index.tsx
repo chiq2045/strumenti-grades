1bd798c5a736aff7fc48da38899b0d9ff9a32b85
chiq2045 <charles.oroko@outlook.com>
2022-12-02 22:04:24 -0800

    chore: update error boundary

--- src/components/error-boundary/index.tsx
+++ src/components/error-boundary/index.tsx
@@ -1,8 +1,33 @@
-import { useRouteError } from 'react-router-dom';
+import { Link as NavLink, useRouteError } from 'react-router-dom';
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Box,
+  Center,
+  Link,
+} from '@chakra-ui/react';
 
 export const ErrorBoundary = () => {
-  const error = useRouteError();
-  console.error(error);
+  const routerError = useRouteError() as { error: Error };
 
-  return <div>Dang!</div>;
+  console.error(routerError);
+
+  return (
+    <div>
+      <Alert status="error">
+        <AlertIcon />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>{routerError.error.message}</AlertDescription>
+      </Alert>
+      <Box h="16">
+        <Center h="100%">
+          <Link role="button" as={NavLink} to="/">
+            Go Home
+          </Link>
+        </Center>
+      </Box>
+    </div>
+  );
 };
